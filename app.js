79e194457d7cb61139a72ce5e3471a02d48b9e86
cerchio22 <cerchiogerrits10@gmail.com>
2022-02-17 14:34:26 +0100

    Co-authored-by: WesleyPost <WesleyPost@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let myOldWindow = window;
 
 function myFunction() {
   alert("heb jij je geluid wel aan?!?!?!!??!11Q/1//");
-  // myNewWindow = window.open("", "_self");
-  // let head = myNewWindow.document.querySelector('head');
-  // head.innerHTML = '<link rel="stylesheet" href="cerwes.css">';
-  // myNewWindow.document.write('<div class="black-rectangle">Dit is een test</div>');
+  /* myNewWindow = window.open("", "_self");
+  let head = myNewWindow.document.querySelector('head');
+  head.innerHTML = '<link rel="stylesheet" href="cerwes.css">';
+  myNewWindow.document.write('<div class="black-rectangle">Dit is een test</div>'); */
   let gif_element = document.querySelector('.gif');
   gif_element.classList.add('gif-show');
 }
@@ -16,13 +16,18 @@ function myFunction1() {
   document.getElementById("loader1").style.visibility = "hidden";
 }
 
+function myFunction2() {
+  document.getElementById("").style.visibility = "visible";
+}
+
+
 function myFunction3() {
  location.href = "irritant.html";
 }
 
 
 /* 
-const popUp = document.querySelector('.po')
+const popUp = document.querySelector('.')
 function popupShow() {
 popUp.setAttribute('style', 'visibility: hidden');
 }
