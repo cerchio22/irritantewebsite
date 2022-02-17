139929fd05b984d59951b5b3a986f5c52218116c
cerchio22 <cerchiogerrits10@gmail.com>
2022-02-17 10:50:59 +0100

    Co-authored-by: WesleyPost <WesleyPost@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ let myOldWindow = window;
 
 
 function myFunction() {
-  alert("zet je geluid aan, jij banaan");
-  // myNewWindow = window.open("", "_self");
-  // let head = myNewWindow.document.querySelector('head');
-  // head.innerHTML = '<link rel="stylesheet" href="cerwes.css">';
-  // myNewWindow.document.write('<div class="black-rectangle">Dit is een test</div>');
-  let gif_element = document.querySelector('.gif');
+  alert("heb jij je geluid wel aan?!?!?!!??!11Q/1//");
+  /* myNewWindow = window.open("", "_self");
+  let head = myNewWindow.document.querySelector('head');
+  head.innerHTML = '<link rel="stylesheet" href="cerwes.css">';
+  myNewWindow.document.write('<div class="black-rectangle">Dit is een test</div>');
+  let gif_element = document.querySelector('.gif'); */
   gif_element.classList.add('gif-show');
 }
 
@@ -19,3 +19,11 @@ function myFunction1() {
 function myFunction3() {
  location.href = "irritant.html";
 }
+
+
+/* THOMAS STUFF
+const popUp = document.querySelector('.popup');
+function popupShow() {
+popUp.setAttribute('style', 'visibility: visible');
+}
+*/
